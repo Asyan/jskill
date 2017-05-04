9b76e72faebb6a48eab001a3e62011a3912b0962
Asyanin Mikhail <creative.doomsday@gmail.com>
2017-05-04 22:32:48 +0300

    minor

--- live-search.js
+++ live-search.js
@@ -1,19 +1,14 @@
-$(document).ready(function() {
-  $("#filter").keyup(function() {
+$(document).ready(function(){
+    $("#filter").keyup(function(){
+        let filter = $(this).val(), count = 0;
 
-    let filter = $(this).val(), count = 0;
-
-    $("nav ul li").each(function() {
-      if ($(this).text().seach(new RegExp(filter, "i")) < 0) {
-          $(this).fadeOut();
-
-      } else {
-          $(this).show();
-          count++;
-      }
+        $("nav ul li").each(function(){
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+            } else {
+                $(this).show();
+                count++;
+            }
+        });
     });
-
-    let numberItems = count;
-    $("#filter-count").text("Numbers of Comments = " + count);
-  });
 });
