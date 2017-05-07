748eaff5b12a82dca54b27fc7ae6e5655a7792e6
Asyanin Mikhail <creative.doomsday@gmail.com>
2017-05-07 05:41:34 +0300

    minor

--- tempApp.js
+++ tempApp.js
@@ -1,14 +1,16 @@
 angular.module('myApp', [])
-.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('myCtrl', function($scope, $http) {
   $scope.cityName = '';
-  var city;
   $scope.getCity = function(link) {
-    $scope.show('http://api.openweathermap.org/data/2.5/weather?q=' + link + '&APPID=8b60be7cf93c48694f9442e0b45f539c&units=metric');
-  }
-    $scope.show = function(city) {
-      $http.get(city)
+    if (link == '') 
+      alert("Enter a city name");
+    else {
+      $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + link + '&APPID=8b60be7cf93c48694f9442e0b45f539c&units=metric')
       .then(function(response) {
         $scope.weather = response.data;  
       })
     };
-}]);
+  }
+});
+
+myCtrl.$inject = ['$scope', '$http'];
