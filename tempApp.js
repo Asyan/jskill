e5a820205530c22bbddd1e96369d5e997e65b704
DESKTOP-KEG92E6\Asyan <creative.doomsday@gmail.com>
2017-09-17 02:10:17 +0300

    add .gitignore, delete unnecessary file & code

--- tempApp.js
+++ tempApp.js
@@ -1,16 +1,19 @@
-angular.module('myApp', [])
-.controller('myCtrl', function($scope, $http) {
-  $scope.cityName = '';
-  $scope.getCity = function(link) {
-    if (link == '') 
-      alert("Enter a city name");
-    else {
-      $http.get('https://api.apixu.com/v1/current.json?key=98fe53844a024b19a7632232170705&q=' + link)
-      .then(function(response) {
-        $scope.weather = response.data;  
-      })
-    };
-  }
-});
+function getTemperature() {
 
-myCtrl.$inject = ['$scope', '$http'];
+
+    angular.module('myApp', [])
+        .controller('myCtrl', function($scope, $http) {
+            $scope.cityName = '';
+            $scope.getCity = function(link) {
+                if (link === '')
+                    alert("Enter a city name");
+                else {
+                    $http.get('https://api.apixu.com/v1/current.json?key=98fe53844a024b19a7632232170705&q=' + link)
+                        .then(function(response) {
+                            $scope.weather = response.data;
+                        })
+                }
+            }
+        });
+}
+getTemperature();
